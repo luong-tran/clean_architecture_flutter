485e7864e0d3b59c1b634c2cd51ba21b20f59833
Luong Tran M. VN.Danang <luong.tran@monstar-lab.com>
2023-10-14 20:17:33 +0700

    Change link default img

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,4 +3,4 @@ const String newsAPIKey = 'a0323320f0534ed28351b4a48394f5a4';
 const String countryQuery = 'us';
 const String categoryQuery = 'general';
 const String kDefaultImage =
-    'https://www.google.com/search?q=default+image&client=firefox-b-d&sxsrf=APq-WBskmtr-ix6NUAqqiHFNpsJX6JSOTg:1650026644151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjEi_qfjJb3AhXvQd8KHd02BKUQ_AUoAXoECAEQAw#imgrc=A0pMe2lq2NT_jM';
+    'https://static.thenounproject.com/png/4974686-200.png';
